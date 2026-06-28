6a764549f4a6e0176513b7d6b85b4869e836d20a
jhonerit <jhonerit.sandro@gmail.com>
2026-06-27 23:06:09 -0500

    correcion de archivo src duplicado que causaba conflictos al ejecutar el proyecto

--- src/src/main/java/com/gas/sistema_gas/Repository/CompraRepository.java
+++
@@ -1,7 +0,0 @@
-package com.gas.sistema_gas.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.gas.sistema_gas.Model.Compra;
-
-public interface CompraRepository extends JpaRepository<Compra, Long> {
-}
