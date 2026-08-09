51b4f05ae1a1b642db782299339f5a64a253d7ac
roy <royagapito248@gmail.com>
2026-08-08 19:54:35 -0500

    test: aislar contexto de la base principal

--- src/test/java/com/gas/sistema_gas/SistemaGasApplicationTests.java
+++ src/test/java/com/gas/sistema_gas/SistemaGasApplicationTests.java
@@ -1,13 +1,27 @@
 package com.gas.sistema_gas;
 
+import com.gas.sistema_gas.integration.InventarioConcurrenciaMySqlTest;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.ContextConfiguration;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
+@ContextConfiguration(
+        initializers = InventarioConcurrenciaMySqlTest.TestDbInitializer.class
+)
 class SistemaGasApplicationTests {
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
 
 	@Test
 	void contextLoads() {
+		String database = jdbcTemplate.queryForObject("SELECT DATABASE()", String.class);
+		assertEquals("sistema_gas_concurrency_test", database);
 	}
 
 }
